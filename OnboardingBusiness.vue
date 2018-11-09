4a903337f65bca629c94a3a2ae18be9d2fbb84b8
ajay14312 <ajaynitm10@gmail.com>
2018-11-09 18:11:04 +0530

    Update OnboardingBusiness.vue

--- OnboardingBusiness.vue
+++ OnboardingBusiness.vue
@@ -6,60 +6,59 @@
             <div class="">
                 <b-form @submit.prevent="onSubmit" @reset="onReset">
                     <b-row>
-                        <b-col sm="6">
-                            <b-form-group id="exampleInputGroup2" label="Business Name" label-for="exampleInput2">
+                        <b-col sm="4">
+                            <b-form-group id="exampleInputGroup2" label="Business Name" label-for="exampleInput2" label-class="asterisk">
                                 <b-form-input id="business_name" type="text" v-model="form.business_name" required>
                                 </b-form-input>
                             </b-form-group>
                         </b-col>
-                        <b-col sm="6">
-                            <b-form-group id="exampleInputGroup2" label="Business Category" label-for="exampleInput2">
+                    </b-row>
+                    <b-row>
+                        <b-col sm="4" class="margin-right20">
+                            <b-form-group id="exampleInputGroup2" label="Business Category" label-for="exampleInput2" label-class="asterisk">
                                 <b-form-input id="business_category" type="text" v-model="form.business_category" required>
                                 </b-form-input>
                             </b-form-group>
                         </b-col>
-                    </b-row>
-                    <div style="clear:both;"></div>
-                    <b-row>
-                        <b-col sm="6">
+                        <b-col sm="4">
                             <b-form-group id="exampleInputGroup2" label="Additional Categories" label-for="exampleInput2">
-                                <b-form-input id="additional_categories" type="text" v-model="form.additional_categories" required>
+                                <b-form-input id="additional_categories" type="text" v-model="form.additional_categories">
                                 </b-form-input>
                             </b-form-group>
                         </b-col>
                     </b-row>
 
                     <b-row>
-                        <b-col sm="6">
-                            <b-form-group id="exampleInputGroup2" label="Business Address" label-for="exampleInput2">
+                        <b-col sm="4">
+                            <b-form-group id="exampleInputGroup2" label="Business Address" label-for="exampleInput2" label-class="asterisk">
                                 <b-form-input id="business_address" type="text" v-model="form.business_address" required>
                                 </b-form-input>
                             </b-form-group>
                         </b-col>
                     </b-row>
                     <b-row>
-                        <b-col sm="6">
-                            <b-form-group id="exampleInputGroup2" label="Country" label-for="exampleInput2">
+                        <b-col sm="4" class="margin-right20">
+                            <b-form-group id="exampleInputGroup2" label="Country" label-for="exampleInput2" label-class="asterisk">
                                 <b-form-input id="country" type="text" v-model="form.country" required>
                                 </b-form-input>
                             </b-form-group>
                         </b-col>
-                        <b-col sm="6">
-                            <b-form-group id="exampleInputGroup2" label="State" label-for="exampleInput2">
+                        <b-col sm="4">
+                            <b-form-group id="exampleInputGroup2" label="State" label-for="exampleInput2" label-class="asterisk">
                                 <b-form-input id="state" type="text" v-model="form.state" required>
                                 </b-form-input>
                             </b-form-group>
                         </b-col>
                     </b-row>
                     <b-row>
-                        <b-col sm="6">
-                            <b-form-group id="exampleInputGroup2" label="City" label-for="exampleInput2">
+                        <b-col sm="4" class="margin-right20">
+                            <b-form-group id="exampleInputGroup2" label="City" label-for="exampleInput2" label-class="asterisk">
                                 <b-form-input id="city" type="text" v-model="form.city" required>
                                 </b-form-input>
                             </b-form-group>
                         </b-col>
-                        <b-col sm="6">
-                            <b-form-group id="exampleInputGroup2" label="Zipcode" label-for="exampleInput2">
+                        <b-col sm="4">
+                            <b-form-group id="exampleInputGroup2" label="Zipcode" label-for="exampleInput2" label-class="asterisk">
                                 <b-form-input id="zipcode" type="text" v-model="form.zipcode" required>
                                 </b-form-input>
                             </b-form-group>
@@ -67,15 +66,15 @@
                     </b-row>
 
                     <b-row>
-                        <b-col sm="6">
-                            <b-form-group id="exampleInputGroup2" label="Business Phone" label-for="exampleInput2">
+                        <b-col sm="4" class="margin-right20">
+                            <b-form-group id="exampleInputGroup2" label="Business Phone" label-for="exampleInput2" label-class="asterisk">
                                 <b-form-input id="business_phone" type="text" v-model="form.business_phone" required>
                                 </b-form-input>
                             </b-form-group>
                         </b-col>
-                        <b-col sm="6">
+                        <b-col sm="4">
                             <b-form-group id="exampleInputGroup2" label="Business Website URL" label-for="exampleInput2">
-                                <b-form-input id="website_url" type="text" v-model="form.website_url" required>
+                                <b-form-input id="website_url" type="text" v-model="form.website_url">
                                 </b-form-input>
                             </b-form-group>
                         </b-col>
@@ -85,7 +84,7 @@
                         Hide Address
                     </b-form-checkbox>
                     <div class="btns">
-                        <b-button type="submit" variant="primary">Submit</b-button>
+                        <b-button type="submit" variant="primary">Continue to Operation Info</b-button>
                         <b-button type="reset" variant="danger">Reset</b-button>
                     </div>
                 </b-form>
@@ -153,6 +152,7 @@ export default {
                     website_url: this.form.website_url
                 })
                 .then(function (response) {
+                    this.$emit('isSubmittedBussInfo', 'ops')
                     if (this.currentLocation.business_detail_flag.length === 0) {
                         this.$router.push({
                             name: "Dashboard"
@@ -168,9 +168,23 @@ export default {
 <style scoped>
 .btn {
     margin-right: 16px;
+    border-radius: .125rem;
 }
 
 .btns {
     margin-top: 30px;
+}
+.btn:focus, .btn-primary:focus {
+     -webkit-box-shadow: 0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12)!important;
+      box-shadow: 0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12)!important; 
+}
+
+.margin-right20 {
+    margin-right: 20%;
+}
+
+.asterisk::after {
+    content: "*";
+    color: red;
 }
 </style>
